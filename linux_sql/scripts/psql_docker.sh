055cb812f28fd31859a9c807c733d35aac3414db
Hongting <min.yinht@gmail.com>
2020-03-13 17:38:14 +0000

    update

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-#usage: ./psql_docker.sh start|stop [password]
+#usage: bash psql_docker.sh start|stop [password]
 
 command=$1
 password=$2
+
+export PGPASSWORD='password'
 
 #check command is start or stop
 if [[ $1 = 'start' && "$#" -eq 2 ]]; then
 	#start docker if docker server is not running
 	systemctl status docker || systemctl start docker
-	echo "docker is running"
+	echo "Docker is running"
 
 	#get latest postgres image
 	docker pull postgres
 
 	#create a new volume if not exist
-	docker volume create pgdata
+	if [ "$(docker volume ls | wc -l)" != 2 ]; then
+		docker volume create pgdata
+		echo "Docker volume is created!"
+	else
+		echo "Docker volume already exists!"
+	fi
 
-	export PGPASSWORD='password'
-
-	#create a container using psql image with name=jrvs-psql
-	docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
-
-	#check if the container `jrvs-psql` is created or not
-	docker container ls -a -f name=jrvs-psql
+        #check if the container `jrvs-psql` is created or not
+        if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" != 2 ]; then
+		#create a container using psql image with name=jrvs-psql
+		docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+		echo "Docker container is created!"
+	else
+		echo "Docker container already exists!"
+	fi
 
 	#check if `jrvs-psql` container is running
-	docker ps -f name=jrvs-psql
-
-	#start a container
-	docker container start jrvs-psql
-	echo "container is running!"
+	if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" != 2 ]; then
+		#start a container
+		docker container start jrvs-psql
+		echo "Docker container starts running!"
+	else
+		echo "Docker container is running!"
+		exit 0
+	fi
 
 elif [[ $1 = 'stop' && "$#" -eq 1 ]]; then
 	#stop the container & docker
@@ -39,7 +50,7 @@ elif [[ $1 = 'stop' && "$#" -eq 1 ]]; then
 	exit 0
 else
 	echo "Error!"
-	echo "usage:./psql_docker.sh start|stop password"
+	echo "usage:bash psql_docker.sh start|stop password"
 	exit 1
 
 fi
