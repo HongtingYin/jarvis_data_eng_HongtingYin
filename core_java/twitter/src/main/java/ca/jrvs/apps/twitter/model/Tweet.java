0b6c492360d49e141bfe1e45f42f62ab02c0ba82
Hongting <min.yinht@gmail.com>
2020-04-22 23:21:07 +0000

    upload Tweet.java

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Tweet.java
@@ -0,0 +1,128 @@
+package ca.jrvs.apps.twitter.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "create_at",
+        "id",
+        "idStr",
+        "text",
+        "entities",
+        "coordinates",
+        "retweetCount",
+        "favoriteCount",
+        "favorited",
+        "retweeted"
+})
+
+public class Tweet {
+
+    @JsonProperty("createAt")
+    private String createAt;
+    @JsonProperty("id")
+    private Long id;
+    @JsonProperty("id_str")
+    private String idStr;
+    @JsonProperty("text")
+    private String text;
+    @JsonProperty("entities")
+    private Entities entities;
+    @JsonProperty("coordinates")
+    private Coordinates coordinates;
+    @JsonProperty("retweetCount")
+    private Integer retweetCount;
+    @JsonProperty("favoriteCount")
+    private Integer favoriteCount;
+    @JsonProperty("favorited")
+    private Boolean favorited;
+    @JsonProperty("retweeted")
+    private Boolean retweeted;
+
+    public Tweet(String text, Coordinates coordinates) {
+        setText(text);
+        setCoordinates(coordinates);
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIdStr() {
+        return idStr;
+    }
+
+    public void setIdStr(String idStr) {
+        this.idStr = idStr;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Entities getEntities() {
+        return entities;
+    }
+
+    public void setEntities(Entities entities) {
+        this.entities = entities;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public Integer getRetweetCount() {
+        return retweetCount;
+    }
+
+    public void setRetweetCount(Integer retweetCount) {
+        this.retweetCount = retweetCount;
+    }
+
+    public Integer getFavoriteCount() {
+        return favoriteCount;
+    }
+
+    public void setFavoriteCount(Integer favoriteCount) {
+        this.favoriteCount = favoriteCount;
+    }
+
+    public Boolean getFavorited() {
+        return favorited;
+    }
+
+    public void setFavorited(Boolean favorited) {
+        this.favorited = favorited;
+    }
+
+    public Boolean getRetweeted() {
+        return retweeted;
+    }
+
+    public void setRetweeted(Boolean retweeted) {
+        this.retweeted = retweeted;
+    }
+}
