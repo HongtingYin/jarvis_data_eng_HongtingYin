a6f60e5aaa995aaa2063127f9668d3d6fb485316
Hongting <min.yinht@gmail.com>
2020-04-22 23:18:17 +0000

    upload Coordinates.java

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/Coordinates.java
@@ -0,0 +1,46 @@
+package ca.jrvs.apps.twitter.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "coordinates",
+        "type"
+})
+
+public class Coordinates {
+
+    @JsonProperty("coordinates")
+    private float[] coordinates;
+    @JsonProperty("type")
+    private String type;
+
+    public Coordinates(float[] coordinates, String type) {
+        setCoordinates(coordinates);
+        setType(type);
+    }
+
+    @JsonProperty("coordinates")
+    public float[] getCoordinates() {
+        return coordinates;
+    }
+
+    @JsonProperty("coordinates")
+    public void setCoordinates(float[] coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+}
