9b7c58287c076b799d35cc3950386bdc9715f302
Hongting <min.yinht@gmail.com>
2020-04-23 21:25:01 +0000

    reformat code

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
@@ -70,14 +70,4 @@ public class TwitterHttpHelper implements HttpHelper {
         }
     }
 
-    //public static void main(String[] args) throws Exception {
-    // String consumerKey = System.getenv("consumerKey");
-    // String consumerSecret = System.getenv("consumerSecret");
-    // String accessToken = System.getenv("accessToken");
-    // String tokenSecret = System.getenv("tokenSecret");
-    // //create components
-    // HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken, tokenSecret);
-    // HttpResponse response = httpHelper.httpPost(new URI (https://api.twitter.com/1.1/statuses/update.json?status=Hello));
-    // System.out.println(EntityUtils.toString(response.getEntity()));
-    //}
 }
