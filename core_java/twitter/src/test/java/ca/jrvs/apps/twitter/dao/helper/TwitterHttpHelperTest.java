10e653b114f9bdad91408f14c1103eb40d1c5b4a
Hongting <min.yinht@gmail.com>
2020-04-23 14:56:59 +0000

    upload TwitterHttpHelperTest.java

---
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelperTest.java
@@ -0,0 +1,25 @@
+package ca.jrvs.apps.twitter.dao.helper;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+
+public class TwitterHttpHelperTest {
+
+    @Test
+    public void httpPost() throws Exception {
+        String consumerKey = System.getenv("consumerKey");
+        String consumerSecret = System.getenv("consumerSecret");
+        String accessToken = System.getenv("accessToken");
+        String tokenSecret = System.getenv("tokenSecret");
+
+        //create components
+        HttpHelper helper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken, tokenSecret);
+        URI uri = new URI("https://api.twitter.com/1.1/statuses/update.json?status=Hello");
+        HttpResponse response = helper.httpPost(uri);
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+}
