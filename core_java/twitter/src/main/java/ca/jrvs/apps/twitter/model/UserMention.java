7171d81310895047b857c907ca281430f78403ea
Hongting <min.yinht@gmail.com>
2020-04-26 21:27:42 +0000

    fix @JsonProperty

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/UserMention.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/model/UserMention.java
@@ -7,26 +7,26 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "id",
-        "idStr",
+        "id_str",
         "indices",
         "name",
-        "screenName"
+        "screen_name"
 })
 
 public class UserMention {
 
     @JsonProperty("id")
     private long id;
-    @JsonProperty("idStr")
-    private String idStr;
+    @JsonProperty("id_str")
+    private String id_str;
     @JsonProperty("indices")
     private int[] indices;
     @JsonProperty("name")
     private String name;
-    @JsonProperty("screenName")
-    private String screenName;
+    @JsonProperty("screen_name")
+    private String screen_name;
 
-    public UserMention(long id, String idStr, int[] indices, String name, String screenname) {
+    public UserMention(long id, String idStr, int[] indices, String name, String screenName) {
         setId(id);
         setIdStr(idStr);
         setIndices(indices);
@@ -34,43 +34,53 @@ public class UserMention {
         setScreenName(screenName);
     }
 
+    @JsonProperty("id")
     public long getId() {
         return id;
     }
 
+    @JsonProperty("id")
     public void setId(long id) {
         this.id = id;
     }
 
+    @JsonProperty("id_str")
     public String getIdStr() {
-        return idStr;
+        return id_str;
     }
 
+    @JsonProperty("id_str")
     public void setIdStr(String idStr) {
-        this.idStr = idStr;
+        this.id_str = idStr;
     }
 
+    @JsonProperty("indices")
     public int[] getIndices() {
         return indices;
     }
 
+    @JsonProperty("indices")
     public void setIndices(int[] indices) {
         this.indices = indices;
     }
 
+    @JsonProperty("name")
     public String getName() {
         return name;
     }
 
+    @JsonProperty("name")
     public void setName(String name) {
         this.name = name;
     }
 
+    @JsonProperty("screen_name")
     public String getScreenName() {
-        return screenName;
+        return screen_name;
     }
 
+    @JsonProperty("screen_name")
     public void setScreenName(String screenName) {
-        this.screenName = screenName;
+        this.screen_name = screenName;
     }
 }
