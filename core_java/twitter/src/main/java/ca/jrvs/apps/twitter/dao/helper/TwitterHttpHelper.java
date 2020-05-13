9b22a4f96707d434078fbcffda514a014f13aabe
Hongting <min.yinht@gmail.com>
2020-05-13 18:24:13 +0000

    fixed the error that consumer if null

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/TwitterHttpHelper.java
@@ -28,13 +28,13 @@ public class TwitterHttpHelper implements HttpHelper {
         httpClient = HttpClientBuilder.create().build();
     }
 
-    public TwitterHttpHelper() {
+   public TwitterHttpHelper() {
         String consumerKey = System.getenv("consumerKey");
         String consumerSecret = System.getenv("consumerSecret");
         String accessToken = System.getenv("accessToken");
         String tokenSecret = System.getenv("tokenSecret");
 
-        OAuthConsumer consumer = new CommonsHttpOAuthConsumer(consumerKey,consumerSecret);
+        this.consumer = new CommonsHttpOAuthConsumer(consumerKey,consumerSecret);
         consumer.setTokenWithSecret(accessToken,tokenSecret);
         httpClient = HttpClientBuilder.create().build();
     }
