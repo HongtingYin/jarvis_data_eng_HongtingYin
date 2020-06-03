8c96c6ea18f5bb4c04ebd64f8e709a5d324d2f93
Hongting <min.yinht@gmail.com>
2020-06-03 15:06:53 +0000

    upload QuoteService.java

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
@@ -0,0 +1,31 @@
+package ca.jrvs.apps.trading.service;
+
+import ca.jrvs.apps.trading.dao.MarketDataDao;
+import ca.jrvs.apps.trading.dao.QuoteDao;
+import ca.jrvs.apps.trading.model.domain.IexQuote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QuoteService {
+    private static final Logger logger = LoggerFactory.getLogger(QuoteService.class);
+
+    private QuoteDao quoteDao;
+    private MarketDataDao marketDataDao;
+
+    public QuoteService(QuoteDao quoteDao, MarketDataDao marketDataDao) {
+        this.quoteDao = quoteDao;
+        this.marketDataDao = marketDataDao;
+    }
+
+    /**
+     * Find an IexQuote
+     *
+     * @param ticker id
+     * @return IexQuote object with the specified ticker
+     * @throws IllegalArgumentException if ticker is invalid
+     */
+    public IexQuote findIexQuoteByTicker (String ticker) {
+        return marketDataDao.findById(ticker).
+                orElseThrow(()-> new IllegalArgumentException(ticker + " is invalid"));
+    }
+}
