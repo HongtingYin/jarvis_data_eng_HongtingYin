cf3f8f6a83d64c7ca282f82ea1c632c7ef5aee45
Hongting <min.yinht@gmail.com>
2020-06-03 15:08:42 +0000

    upload QuoteDao.java and JdbcCrudDao.java

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/QuoteDao.java
@@ -0,0 +1,55 @@
+package ca.jrvs.apps.trading.dao;
+
+import ca.jrvs.apps.trading.model.domain.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class QuoteDao extends JdbcCrudDao<Quote, String> {
+
+    private static final String TABLE_NAME = "quote";
+    private static final String ID_COLUMN_NAME = "ticker";
+
+    private static final Logger logger = LoggerFactory.getLogger(QuoteDao.class);
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert simpleJdbcInsert;
+
+    @Autowired
+    public QuoteDao(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName(TABLE_NAME);
+    }
+
+
+    @Override
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Override
+    public SimpleJdbcInsert getSimpleJdbcInsert() {
+        return simpleJdbcInsert;
+    }
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
+    @Override
+    public String getIdColumnName() {
+        return ID_COLUMN_NAME;
+    }
+
+    @Override
+    Class<Quote> getEntityClass() {
+        return Quote.class;
+    }
+
+}
